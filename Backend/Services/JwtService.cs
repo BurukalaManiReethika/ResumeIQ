b0726744e4a5f70980518a2d7365a2fa61a5a608
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-17 17:40:45 +0530

    Update JwtService.cs

--- Backend/Services/JwtService.cs
+++ Backend/Services/JwtService.cs
@@ -1,9 +1,23 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
 namespace ResumeIQ.Services;
 
 public class JwtService
 {
-    public string GenerateToken()
+    public string GenerateToken(string email)
     {
-        return Guid.NewGuid().ToString();
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Email, email)
+        };
+
+        return new JwtSecurityTokenHandler()
+            .WriteToken(
+                new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddDays(1)
+                )
+            );
     }
 }
