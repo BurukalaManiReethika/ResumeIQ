04cafd6aecd23d36e1e1c0149dd7a7d84acaa20a
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-17 17:04:11 +0530

    Create Resume.cs

---
+++ Backend/Models/Resume.cs
@@ -0,0 +1,14 @@
+namespace ResumeIQ.Models;
+
+public class Resume
+{
+    public int Id { get; set; }
+
+    public string CandidateName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Skills { get; set; }
+
+    public int ATSScore { get; set; }
+}
