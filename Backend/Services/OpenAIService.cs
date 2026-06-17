ca04b82c5947e8e1010fa4235a590705af43d41a
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-17 17:24:39 +0530

    Create OpenAIService.cs

---
+++ Backend/Services/OpenAIService.cs
@@ -0,0 +1,10 @@
+namespace ResumeIQ.Services;
+
+public class OpenAIService
+{
+    public string AnalyzeResume(string resumeText)
+    {
+        return
+        "Strong technical background. Add more projects and certifications.";
+    }
+}
